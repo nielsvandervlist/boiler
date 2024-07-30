4700c260372f749cf07de56f324bde281e888ee9
Niels van der Vlist <nielsvandervlist@leiderdorpinstruments.nl>
2024-07-30 09:17:14 +0200

    Add layout changes

--- app/layout.js
+++ app/layout.js
@@ -5,10 +5,10 @@ import SessionProviderWrapper from "@/components/SessionProviderWrapper";
 import Navbar from '@/components/Navbar';
 import Footer from "@/components/Footer";
 import Container from "@/components/Container";
-import { usePathname } from 'next/navigation';
-import { AnimatePresence, motion } from 'framer-motion';
+import {usePathname} from 'next/navigation';
+import {AnimatePresence, motion} from 'framer-motion';
 
-export default function RootLayout({ children, session }) {
+export default function RootLayout({children, session}) {
     const pathname = usePathname();
 
     return (
@@ -16,22 +16,22 @@ export default function RootLayout({ children, session }) {
         <body>
         <main className={'min-h-[100vh] flex flex-col'}>
             <SessionProviderWrapper session={session}>
-                <Navbar />
+                <Navbar/>
                 <AnimatePresence mode={"wait"}>
-                <motion.div
-                    key={pathname}
-                    initial={{ opacity: 0, rotate: 10, scale: 0.8 }}
-                    animate={{ opacity: 1, rotate: 0, scale: 1 }}
-                    exit={{ opacity: 0, rotate: -10, scale: 0.8 }}
-                    transition={{ duration: 0.7, ease: [0.43, 0.13, 0.23, 0.96] }}
-                    className={'flex justify-center items-center min-w-8'}
-                >
-                    <Container>
-                        {children}
-                    </Container>
-                </motion.div>
+                    <motion.div
+                        key={pathname}
+                        initial={{opacity: 0, rotate: 10, scale: 0.8}}
+                        animate={{opacity: 1, rotate: 0, scale: 1}}
+                        exit={{opacity: 0, rotate: -10, scale: 0.8}}
+                        transition={{duration: 0.7, ease: [0.43, 0.13, 0.23, 0.96]}}
+                        className={'flex justify-center items-center min-w-8'}
+                    >
+                        <Container>
+                            {children}
+                        </Container>
+                    </motion.div>
                 </AnimatePresence>
-                <Footer />
+                <Footer/>
             </SessionProviderWrapper>
         </main>
         </body>
